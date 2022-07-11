10be0d2fa1a798440b1807871e506217951b2c87
Your Name <you@example.com>
2022-07-11 10:28:01 +0700

    Variable

---
+++ index.ts
@@ -0,0 +1,4 @@
+import {FizzBuzz} from "./variable";
+
+let fizzbuzz = new FizzBuzz(15);
+console.log(fizzbuzz.message)
---
+++ variable.ts
@@ -0,0 +1,18 @@
+export class FizzBuzz {
+    message: string;
+
+    constructor(n: number) {
+        let isBuzz = n % 3 == 0;
+        let isFizz = n % 5 == 0;
+
+        if (isBuzz && isFizz) {
+            this.message = 'FizzBuzz';
+        } else if (isFizz) {
+            this.message = 'Fizz';
+        } else if(isBuzz) {
+            this.message = 'Buzz';
+        } else {
+            this.message = n + ''
+        }
+    }
+}
